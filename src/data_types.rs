75cbfdb05ed707ced90dd0bbbaa996a7e0559281
Stephen Cameron <stephen@oxt.ca>
2024-11-10 14:11:41 -0500

    add in parse tests

--- src/data_types.rs
+++ src/data_types.rs
@@ -169,3 +169,182 @@ pub trait Parse {
         None
     }
 }
+
+struct ParseTests {}
+
+impl Parse for ParseTests {
+    fn parse(map: &serde_json::Map<String, serde_json::Value>) -> Self {
+        ParseTests {}
+    }
+}
+
+    #[test]
+    fn test_bool_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(true));
+        let result: Option<bool> = ParseTests::bool_parse(&map, vec!["key1", "key2"]);
+        assert_eq!(result, Some(true));
+    }
+
+    #[test]
+    fn test_bool_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(true));
+        let result = ParseTests::bool_parse(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_string_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!("value1"));
+        let result = ParseTests::string_parse(&map, vec!["key1", "key2"]);
+        assert_eq!(result, Some("value1".to_string()));
+    }
+
+    #[test]
+    fn test_string_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!("value1"));
+        let result = ParseTests::string_parse(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_f64_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(1.23));
+        let result = ParseTests::f64_parse(&map, vec!["key1", "key2"]);
+        assert_eq!(result, Some(1.23));
+    }
+
+    #[test]
+    fn test_f64_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(1.23));
+        let result = ParseTests::f64_parse(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_i64_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(123));
+        let result = ParseTests::i64_parse(&map, vec!["key1", "key2"]);
+        assert_eq!(result, Some(123));
+    }
+
+    #[test]
+    fn test_i64_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(123));
+        let result = ParseTests::i64_parse(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }
+
+    /*#[test]
+    fn test_object_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        let mut inner_map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map.insert("inner_key".to_string(), serde_json::json!("inner_value"));
+        map.insert("key1".to_string(), serde_json::json!(inner_map));
+        let result = ParseTests::object_parse::<HashMap<String, String>>(
+            &map,
+            vec!["key1", "key2"],
+        );
+        assert_eq!(
+            result,
+            Some(HashMap::from([("inner_key".to_string(), "inner_value".to_string())]))
+        );
+    }
+
+    #[test]
+    fn test_object_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        let mut inner_map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map.insert("inner_key".to_string(), serde_json::json!("inner_value"));
+        map.insert("key1".to_string(), serde_json::json!(inner_map));
+        let result = ParseTests::object_parse::<HashMap<String, String>>(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_array_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        let mut inner_map1: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map1.insert("inner_key".to_string(), serde_json::json!("inner_value1"));
+        let mut inner_map2: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map2.insert("inner_key".to_string(), serde_json::json!("inner_value2"));
+        map.insert(
+            "key1".to_string(),
+            serde_json::json!(vec![inner_map1, inner_map2]),
+        );
+        let result =
+        ParseTests::array_parse::<HashMap<String, String>>(&map, vec!["key1", "key2"]);
+        assert_eq!(
+            result,
+            Some(vec![
+                HashMap::from([("inner_key".to_string(), "inner_value1".to_string())]),
+                HashMap::from([("inner_key".to_string(), "inner_value2".to_string())])
+            ])
+        );
+    }
+
+    #[test]
+    fn test_array_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        let mut inner_map1: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map1.insert("inner_key".to_string(), serde_json::json!("inner_value1"));
+        let mut inner_map2: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map2.insert("inner_key".to_string(), serde_json::json!("inner_value2"));
+        map.insert(
+            "key1".to_string(),
+            serde_json::json!(vec![inner_map1, inner_map2]),
+        );
+        let result = ParseTests::array_parse::<HashMap<String, String>>(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }*/
+
+    #[test]
+    fn test_array_i64_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(vec![1, 2, 3]));
+        let result = ParseTests::array_i64_parse(&map, vec!["key1", "key2"]);
+        assert_eq!(result, Some(vec![1, 2, 3]));
+    }
+
+    #[test]
+    fn test_array_i64_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        map.insert("key1".to_string(), serde_json::json!(vec![1, 2, 3]));
+        let result = ParseTests::array_i64_parse(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_hashmap_parse_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        let mut inner_map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map.insert("key1".to_string(), serde_json::json!(1.23));
+        inner_map.insert("key2".to_string(), serde_json::json!(4.56));
+        map.insert("size".to_string(), serde_json::json!(inner_map));
+        let result = ParseTests::hashmap_parse(&map, vec!["size", "key2"]);
+        assert_eq!(
+            result,
+            Some(HashMap::from([
+                ("key1".to_string(), 1.23),
+                ("key2".to_string(), 4.56)
+            ]))
+        );
+    }
+
+    #[test]
+    fn test_hashmap_parse_not_found() {
+        let mut map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        let mut inner_map: serde_json::Map<String, serde_json::Value> = serde_json::Map::new();
+        inner_map.insert("key1".to_string(), serde_json::json!(1.23));
+        inner_map.insert("key2".to_string(), serde_json::json!(4.56));
+        map.insert("size".to_string(), serde_json::json!(inner_map));
+        let result = ParseTests::hashmap_parse(&map, vec!["key2"]);
+        assert_eq!(result, None);
+    }
